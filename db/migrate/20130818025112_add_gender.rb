61c506d18e9005327e10ea5a551f3a59b4912a53
martiantim <martiantim@gmail.com>
2014-02-05 00:29:20 -0800

    migration update

--- db/migrate/20130818025112_add_gender.rb
+++ db/migrate/20130818025112_add_gender.rb
@@ -1,6 +1,6 @@
 class AddGender < ActiveRecord::Migration
   def up
-    add_column :users, :gender, :string, :limit => 1, :null => false, :defualt => 'U'
+    add_column :users, :gender, :string, :limit => 1, :null => false, :default => 'U'
     
     User.find_by_name("Tim").update_attribute(:gender, 'M')
     User.find_by_name("April").update_attribute(:gender, 'F')
